2e6daae2e41b8e274ca1c8bf2d1a224d52df9352
Morre Thierry <thierry.morre@cognitic.be>
2022-07-11 16:48:49 +0200

    Correction des insertions

--- EF6CourseSolution.App/Program.cs
+++ EF6CourseSolution.App/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace EF6CourseSolution.App
+﻿using EF6CourseSolution.Context;
+using EF6CourseSolution.Entities;
+using E = EF6CourseSolution.Entities;
+
+namespace EF6CourseSolution.App
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProjectContext projectContext = new ProjectContext();
+
+            Project project = new Project() { Name = "Demo Ef Core 6", DeadLine = DateTime.Now.AddDays(1) };
+            project.Tasks.Add(new E.Task() { Name = "Create Project", Description = "Create The project using EF Core 6" });
+            project.Tasks.Add(new E.Task() { Name = "Deploy Project", Description = "Deploy the database" });
+
+            projectContext.Add(project);
+            projectContext.SaveChanges();
+
+            User user = new User() { LastName = "Doe", FirstName = "Jane", Email = "doe.jane@gmail.com", Passwd = "Test1234=" };
+            project.Users.Add(user);
+
+            projectContext.SaveChanges();
+            foreach(E.Task task in project.Tasks)
+            {
+                task.User = user;
+            }
+
+            projectContext.SaveChanges();
         }
     }
 }
